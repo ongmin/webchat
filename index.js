a3947e1a150713171400f78131b6611852f03c11
Min Ong <xinmin.ong@gmail.com>
2015-12-30 18:39:35 +0100

    Refactor to ES2016 and updated correspondingly Package.JSON dependecies and scripts

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+import express from 'express'
+import http from 'http'
+import socketIo from 'socket.io'
+
+var app = express()
+var server = http.Server(app)
+var io = socketIo(server)
+
+// var express = require('express')
+// var app = express()
+// var http = require('http').Server(app)
+// var io = require('socket.io')(http)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
@@ -17,6 +26,6 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(3000, function () {
+server.listen(3000, function () {
   console.log('listening on *:3000')
 })
